d6eb4f6506476d33eb212e5d888bd8f30e613b9f
YooHyeokJin <yhjmir@naver.com>
2023-01-14 04:23:26 +0900

    feat: ImageMouseEnter

---
+++ Assets/Script/Menu/UserInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class UserInfo
+{
+    public static int money;
+    public static int itemCount = 131;
+    public static bool[] userItem = new bool[itemCount];
+}
