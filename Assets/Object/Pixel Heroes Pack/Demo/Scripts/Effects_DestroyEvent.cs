ef029a162d1e3a9e1906ce628a9776e36a1ac290
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-01-17 09:49:56 +0900

    design: 캐릭터 에셋 임폴트

---
+++ Assets/Object/Pixel Heroes Pack/Demo/Scripts/Effects_DestroyEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effects_DestroyEvent : MonoBehaviour
+{
+    public void destroyEvent()
+    {
+        Destroy(gameObject);
+    }
+}
