f478338531bde38bf710e2b90852bf0e0f2490d7
yhj0329 <wlrnfhd0329@gmail.com>
2023-03-10 10:50:52 +0900

    feat:Gralic create

--- Assets/Script/Equipment/Weapon/Gralic.cs
+++ Assets/Script/Equipment/Weapon/Gralic.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class Gralic : MonoBehaviour
 {
     Weapon Stat;
+    bool isExist;
     private GameObject player;
     float timer = 0;
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
-        
+        isExist = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        transform.position = GameManager.instance.player.transform.position + new Vector3(0, 0.5f, 0);
+        Debug.Log("Gralic");
     }
 
     public void SpawnGralic(GameObject objPre)
     {
+        Debug.Log(objPre.activeSelf);
+        if(!isExist)
+        {
+            GameObject newobj = Instantiate(objPre);
+            newobj.transform.position = GameManager.instance.player.transform.position + new Vector3(0, 0.5f, 0); // 1�ʵڿ� ������ �ֱ� ����.
+            isExist = true;
+        }
         
     }
 }
