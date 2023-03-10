fc6848f4b3ef8e541609dbfcc2fdecf9c88cf979
yhj0329 <wlrnfhd0329@gmail.com>
2023-03-10 17:58:10 +0900

    feat: Gralic Cooldown

--- Assets/Script/Equipment/Weapon/Gralic.cs
+++ Assets/Script/Equipment/Weapon/Gralic.cs
@@ -5,30 +5,33 @@ using UnityEngine;
 public class Gralic : MonoBehaviour
 {
     Weapon Stat;
-    bool isExist;
-    private GameObject player;
+    bool isExist = false;
+    GameObject newobj;
     float timer = 0;
-
-    void Awake()
-    {
-        isExist = false;
-    }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = GameManager.instance.player.transform.position + new Vector3(0, 0.5f, 0);
-        Debug.Log("Gralic");
     }
 
     public void SpawnGralic(GameObject objPre)
     {
-        Debug.Log(objPre.activeSelf);
-        if(!isExist)
+        timer += Time.deltaTime;
+        if (timer > objPre.GetComponent<Weapon>().WeaponTotalStats[((int)Enums.WeaponStat.Cooldown)])
         {
-            GameObject newobj = Instantiate(objPre);
-            newobj.transform.position = GameManager.instance.player.transform.position + new Vector3(0, 0.5f, 0); // 1�ʵڿ� ������ �ֱ� ����.
-            isExist = true;
+            if(!isExist)
+            {
+                newobj = Instantiate(objPre);
+                newobj.transform.position = GameManager.instance.player.transform.position + new Vector3(0, 0.5f, 0);
+                isExist = true;
+            }
+            else
+            {
+                Destroy(newobj);
+                isExist = false;
+            }
+            timer = 0;
         }
         
     }
