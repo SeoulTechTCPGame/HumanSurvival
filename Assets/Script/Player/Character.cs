2309a7a4cf431fa9211b163bda7351345dccac16
YooHyeokJin <yhjmir@naver.com>
2023-03-24 18:54:03 +0900

    chore: main pull시 충돌 해결

--- Assets/Script/Player/Character.cs
+++ Assets/Script/Player/Character.cs
@@ -59,7 +59,7 @@ public class Character : EquipmentManagementSystem,IDamageable
 
         characterData = Resources.Load<CharacterScriptableObject>(resourceName);
         Debug.Log(characterData.MagnetBonus);
-        CharacterStats = new float[21] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 70, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        CharacterStats = new float[21] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         Weapons = new List<Weapon>();
         Accessories = new List<Accessory>();
         TransWeaponIndex = Enumerable.Repeat<int>(-1, 13).ToArray<int>();
