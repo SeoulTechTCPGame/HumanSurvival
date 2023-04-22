cfbafacc20bd816a76784991b8ae45235c4e3dac
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-04-22 15:53:16 +0900

    refactor: cs 한글화

--- Assets/Scenes/Script/StageSelection/StageOfSelectionScene.cs
+++ Assets/Scenes/Script/StageSelection/StageOfSelectionScene.cs
@@ -15,10 +15,10 @@ public class StageOfSelectionScene : MonoBehaviour
     public void LoadMapData(MapScriptableObject mapData)
     {
         textMeshes[0].SetText(mapData.StageName);
-        textMeshes[1].SetText("Time: " + mapData.PlayTime);
-        textMeshes[2].SetText("DoubleSpeed: " + mapData.DoubleSpeed);
-        textMeshes[3].SetText("GoldCoinBonus: " + mapData.GoldCoinBonus);
-        textMeshes[4].SetText("LuckBonus: " + mapData.LuckBonus);
-        textMeshes[5].SetText("ExperienceBonus: " + mapData.ExperienceBonus);
+        textMeshes[1].SetText("게임 시간: " + mapData.PlayTime);
+        textMeshes[2].SetText("배속: " + mapData.DoubleSpeed);
+        textMeshes[3].SetText("골드 보너스: " + mapData.GoldCoinBonus);
+        textMeshes[4].SetText("행운 보너스: " + mapData.LuckBonus);
+        textMeshes[5].SetText("경험치 보너스: " + mapData.ExperienceBonus);
     }
 }
