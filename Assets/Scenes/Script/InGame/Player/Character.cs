dc9bf535def9276bb090ce12127531f1cf77d077
YooHyeokJin <wlrnfyd0329@gmail.com>
2023-05-07 02:06:06 +0900

    feat:Character Armor 구현

--- Assets/Scenes/Script/InGame/Player/Character.cs
+++ Assets/Scenes/Script/InGame/Player/Character.cs
@@ -8,6 +8,7 @@ public class Character : MonoBehaviour, IDamageable
     private bool isDead;
     private float currentHp;
     private float maxHp;
+    private float armor;
 
     private float mExp;
     private int mMaxExp;
@@ -15,15 +16,18 @@ public class Character : MonoBehaviour, IDamageable
     public float hpRegenerationTimer;
 
     void Start()
-    {   
+    {
         mExp = 0;
         mMaxExp = 100;
         currentHp = GameManager.instance.characterData.MaxHealth * GameManager.instance.CharacterStats[(int)Enums.Stat.MaxHealth];
         maxHp = GameManager.instance.characterData.MaxHealth * GameManager.instance.CharacterStats[(int)Enums.Stat.MaxHealth];
-
+        armor = GameManager.instance.CharacterStats[(int)Enums.Stat.Armor];
     }
     private void Update()
     {
+        // 장신구 레벨업시 스텟이 적용되려면 Start가 아니라 Update에서도 계속 값을 업데이트해야지 적용된다.
+        maxHp = GameManager.instance.characterData.MaxHealth * GameManager.instance.CharacterStats[(int)Enums.Stat.MaxHealth];
+        armor = GameManager.instance.CharacterStats[(int)Enums.Stat.Armor];
         //체력 재생력
         hpRegenerationTimer += Time.deltaTime * GameManager.instance.CharacterStats[(int)Enums.Stat.Recovery];
         if (hpRegenerationTimer > 1f)
@@ -33,10 +37,10 @@ public class Character : MonoBehaviour, IDamageable
 
         }
     }
-        public void RestoreHealth(float amount)
+    public void RestoreHealth(float amount)
     {
-        if(currentHp<maxHp)
-        { 
+        if (currentHp < maxHp)
+        {
             currentHp += amount;
             if (currentHp > maxHp) currentHp = maxHp;
             HpBar.SetState(currentHp, maxHp);
@@ -45,8 +49,17 @@ public class Character : MonoBehaviour, IDamageable
     public void TakeDamage(float damage, int weaponIndex)
     {
         Debug.Log(currentHp);
+        Debug.Log(damage);
+        Debug.Log(armor);
         if (isDead == true) return;
-        currentHp -= Time.deltaTime*damage*2;
+        if (damage - armor <= 0)
+        {
+            currentHp -= Time.deltaTime * 0 * 2;
+        }
+        else
+        {
+            currentHp -= Time.deltaTime * (damage - armor) * 2;
+        }
         if (currentHp <= 0)
         {
             GameManager.instance.GameOverPanelUp();
