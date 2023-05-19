314bb2aff485bbe108c87364c806a3d612fc561a
pianoop <kphjjh723@gmail.com>
2023-05-19 19:54:22 +0900

    chore: edit weapon index

--- Assets/Scenes/Script/InGame/Common/GameManager.cs
+++ Assets/Scenes/Script/InGame/Common/GameManager.cs
@@ -91,6 +91,7 @@ public class GameManager : MonoBehaviour
         CharacterStats[(int)Enums.Stat.Banish] = characterData.Banish;
         CharacterStats[(int)Enums.Stat.Ommi] = characterData.Ommi;
         CharacterStats[(int)Enums.Stat.Reflection] = characterData.Reflection;
+
         equipManageSys.Set(characterData.startingWeapon);
         UpdateLuck(CharacterStats[(int)Enums.Stat.Luck]);
     }
