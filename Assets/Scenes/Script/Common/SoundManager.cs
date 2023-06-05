a338ccb71586c93f6b1f77ffca2d0f2e75c6a4a7
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-06-05 18:57:10 +0900

    feat: Button click sound function

--- Assets/Scenes/Script/Common/SoundManager.cs
+++ Assets/Scenes/Script/Common/SoundManager.cs
@@ -10,6 +10,9 @@ public class SoundManager : MonoBehaviour
     public float bgmVolume = 1.0f; // BGM 볼륨
     public float soundEffectVolume = 1.0f; // 사운드 이펙트 볼륨
 
+    public AudioClip buttonSoundClip; // 버튼 소리 파일
+    private AudioSource audioSource; // 소리를 재생할 오디오 소스
+
     private void Awake()
     {
         if (instance == null)
@@ -22,6 +25,8 @@ public class SoundManager : MonoBehaviour
             return;
         }
         DontDestroyOnLoad(gameObject);
+        // 오디오 소스 설정
+        audioSource = GetComponent<AudioSource>();
     }
 
     // BGM 재생
@@ -36,5 +41,10 @@ public class SoundManager : MonoBehaviour
     {
         // 마스터 볼륨과 사운드 이펙트 볼륨 곱 연산 적용
         float volume = soundEffectVolume;
+    }
+    //버튼 클릭 사운드 재생
+    public void PlayButtonSound()
+    {
+        audioSource.PlayOneShot(buttonSoundClip);
     }
 }
