35f77bef7acf1e87c845cb0161b64357fea2aff6
Jeon-YJ1004 <dudwn5374@naver.com>
2023-06-23 19:54:43 +0900

    fix: 맵 오브젝트 layer

--- Assets/Scenes/Script/InGame/Common/DestructibleObject.cs
+++ Assets/Scenes/Script/InGame/Common/DestructibleObject.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public class DestructibleObject : MonoBehaviour,IDamageable
 {
+    
+    public enum EDestructibleType { TreasureBox, Bottle}
+    public EDestructibleType Type;
+
     public void TakeDamage(float damage, int weaponIndex)
     {
         //position 위치에 Drop 생성
-        gameObject.GetComponent<DropSystem>().OnDrop(gameObject.transform.position);
+        switch (Type)
+        {
+            case EDestructibleType.TreasureBox:
+                //보물상자 획득 시 호출할 함수
+                Debug.Log("보물상자 드롭");
+                break;
+            case EDestructibleType.Bottle:
+                
+                gameObject.GetComponent<DropSystem>().OnDrop(gameObject.transform.position);
+                break;
+        }
         Destroy(gameObject);
     }
 }
--- Assets/Scenes/Script/InGame/Player/PlayerCollector.cs
+++ Assets/Scenes/Script/InGame/Player/PlayerCollector.cs
@@ -5,6 +5,10 @@ public class PlayerCollector : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.TryGetComponent(out ICollectible collectible))
+        {
+            Debug.Log("부딪힘");
             collectible.Collect();
+        }
+            
     }
 }
