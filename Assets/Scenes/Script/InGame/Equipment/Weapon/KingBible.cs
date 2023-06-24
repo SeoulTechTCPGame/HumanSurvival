23503361188a4e143bcda652278539f34cda3e51
yhj0329 <wlrnfhd0329@gmail.com>
2023-06-25 02:46:52 +0900

    feat: KingBible 완성

--- Assets/Scenes/Script/InGame/Equipment/Weapon/KingBible.cs
+++ Assets/Scenes/Script/InGame/Equipment/Weapon/KingBible.cs
@@ -7,15 +7,13 @@ public class KingBible : Weapon
     private float mDistance = 4f;  // 오브젝트와 타겟 사이의 거리
     private float mSpeed = 120f;  // 공전 속도
     private float mTimer = 0;
-    private Vector3 pos;
 
     private void Update()
     {
-        transform.RotateAround(GameManager.instance.Player.transform.position, Vector3.back, mSpeed * WeaponTotalStats[((int)Enums.EWeaponStat.ProjectileSpeed)] * Time.deltaTime);
-    }
-    private Vector3 getStartPosition(Vector3 pos)
-    {
-        return pos + Vector3.up * mDistance;
+        transform.parent.transform.position = GameManager.instance.Player.transform.position;
+        transform.parent.transform.Translate(Vector3.down);
+        transform.parent.transform.Rotate(Vector3.back * mSpeed * WeaponTotalStats[((int)Enums.EWeaponStat.ProjectileSpeed)] * Time.deltaTime / WeaponTotalStats[((int)Enums.EWeaponStat.Amount)]);
+
     }
     public override void Attack()
     {
@@ -27,22 +25,20 @@ public class KingBible : Weapon
 
         if(!mbExist && mTimer > WeaponTotalStats[((int)Enums.EWeaponStat.Cooldown)])
         {
-            // for (int i = 0; i < WeaponTotalStats[((int)Enums.EWeaponStat.Amount)]; i++)
-            // {
-            //     mNewObj = Instantiate(objPre);
-            //     mNewObj.transform.parent = GameObject.Find("SkillFiringSystem").transform;
-
-            //     Vector3 spawnPosition = getStartPosition(GameManager.instance.Player.transform.position);
-            //     //여러 개 동시 발사 시 시작 위치를 다르게 설정
-            //     mNewObj.transform.position = spawnPosition; //시작 위치
-            // }
-
-            mNewObj = Instantiate(objPre);
+            
+            mNewObj = new GameObject("KingBibles");
             mNewObj.transform.parent = GameObject.Find("SkillFiringSystem").transform;
 
-            Vector3 spawnPosition = getStartPosition(GameManager.instance.Player.transform.position);
-            //여러 개 동시 발사 시 시작 위치를 다르게 설정
-            mNewObj.transform.position = spawnPosition; //시작 위치
+            for (int i = 0; i < WeaponTotalStats[((int)Enums.EWeaponStat.Amount)]; i++)
+            {
+                GameObject kingBible = Instantiate(objPre);
+                kingBible.transform.parent = GameObject.Find("KingBibles").transform;
+
+                Vector3 rotVec = Vector3.forward * 360 * i / WeaponTotalStats[((int)Enums.EWeaponStat.Amount)];
+                //여러 개 동시 발사 시 시작 위치를 다르게 설정
+                kingBible.transform.Rotate(rotVec);
+                kingBible.transform.Translate(kingBible.transform.up * mDistance, Space.World);
+            }
             mbExist = true;
             mTimer = 0;
         }
