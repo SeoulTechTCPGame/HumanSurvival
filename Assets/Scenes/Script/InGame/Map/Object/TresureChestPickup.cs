3197dfa32209200d8dbf376a398ace44e7fa81b2
pianop <kphjjh723@gmail.com>
2023-07-15 15:46:14 +0900

    chore: connect mTresureChestUI to TreasureChestUI gameobject in Ingame scene

--- Assets/Scenes/Script/InGame/Map/Object/TresureChestPickup.cs
+++ Assets/Scenes/Script/InGame/Map/Object/TresureChestPickup.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class TresureChestPickup : MonoBehaviour, ICollectible
 {
-    [SerializeField] GameObject mTresureChestUI;
+    private GameObject mTresureChestUI;
+
+    private void Start()
+    {
+        mTresureChestUI = GameObject.Find("TreasureChestUI");
+    }
     public void Collect()
     {
-        Debug.Log("보물 상자 획득");
         mTresureChestUI.GetComponent<TreasureChest>().LoadChestUI();
         Destroy(gameObject);
     }
