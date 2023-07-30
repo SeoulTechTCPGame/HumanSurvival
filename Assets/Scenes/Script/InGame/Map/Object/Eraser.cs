e57a7aeb581d83a3e379be3068a507b3cdda79c5
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-07-30 17:58:59 +0900

    chore: enprogress eraser's algorithm

--- Assets/Scenes/Script/InGame/Map/Object/Eraser.cs
+++ Assets/Scenes/Script/InGame/Map/Object/Eraser.cs
@@ -3,41 +3,29 @@ using UnityEngine;
 public class Eraser : MonoBehaviour, ICollectible
 {
     public float Damage = 666;
-    public float[] Area = {15, 8};
+    public float[] Area = {18, 10};
     [SerializeField] AudioClip mClip;
 
     public void Collect()
     {
-        Vector3[] enemyPositions = FindAllEnemies(Area[0], Area[1]);
-        foreach (Vector3 enemyPosition in enemyPositions)
-        {
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(enemyPosition, 0.1f, LayerMask.GetMask("Monster"));
-            foreach (Collider2D collider in colliders)
-            {
-                Enemy enemy = collider.GetComponent<Enemy>();
-                if (enemy != null)
-                {
-                    enemy.TakeDamage(Damage, -1);
-                }
-            }
-        }
         gameObject.SetActive(false);
         SoundManager.instance.PlaySoundEffect(mClip);
+
+        Collider2D[] enemies = FindAllEnemies(Area[0], Area[1]);
+        if (enemies.Length == 0) { return; }
+        foreach (Collider2D enemy in enemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(Damage, -1);
+        }
     }
-    private Vector3[] FindAllEnemies(float width, float height)
+    private Collider2D[] FindAllEnemies(float width, float height)
     {
         Collider2D[] enemies = Physics2D.OverlapAreaAll(
             GameManager.instance.Player.transform.position + Vector3.left * width + Vector3.up * height,
             GameManager.instance.Player.transform.position + Vector3.right * width + Vector3.down * height,
             LayerMask.GetMask("Monster")
             );
-        
-        Vector3[] positions = new Vector3[enemies.Length];
-        for (int i = 0; i < enemies.Length; i++)
-        {
-            positions[i] = enemies[i].transform.position;
-        }
 
-        return positions;
+        return enemies;
     }
 }
